2096528275142704b7bebfce7debcfef9a4da7ba
saber <1999ms@live.fr>
2024-03-08 10:16:54 +0100

    home

---
+++ src/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+function WelcomePage() {
+  return (
+    <div>
+      <h1>Welcome to the Job Listing App!</h1>
+      <p>Start exploring job opportunities now.</p>
+    </div>
+  );
+}
+
+export default WelcomePage;
